257d70d41c55cd69a743c1d75aeb18fed961324e
Daniel Malagarriga <daniel.malagarriga@upc.edu>
2021-01-18 16:03:27 +0100

    adding gitignore

--- README.md
+++
@@ -1,5 +0,0 @@
-# Annotate ecography images
-A Dash app to annotate ecography images
-
-## Running the app
-Run python index.py to run the app
--- app.py
+++
@@ -1,6 +0,0 @@
-import dash
-import dash_bootstrap_components as dbc
-
-external_stylesheets = [dbc.themes.BOOTSTRAP, "assets/image_annotation_style.css"]
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True)
-server = app.server
